0b61e57bd24c08c7f57f1037942cf27eb7fffcb6
Cesar Urdaneta <ing.cesar.u@gmail.com>
2023-06-15 02:36:07 -0400

    Subida inicial

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+const NavBar = () => {
+        return (
+            <Nav>
+                <h3>HELADERIA S&H SWEET AND HEALTHY</h3>
+                <div>
+                    <button>Crema y Chocolate</button>
+                    <button>Fresas y Crema</button>
+                    <button>Sueños de Avellana</button>
+                </div>
+            </Nav>
+        )
+}
