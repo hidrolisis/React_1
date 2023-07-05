198d96f63dda45980b4a16dc4a272aa31963e99e
Cesar Urdaneta <ing.cesar.u@gmail.com>
2023-07-05 02:17:23 -0400

    Agregando carrito

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,11 @@
+import cart from './assets/carrito-de-compras.png';
+
+const CartWidget = () => {
+    return (
+        <div>
+            <img id='logo' src={cart} alt='cart-widget' />
+        </div>
+    )
+}
+
+export default CartWidget
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,21 @@
+import CartWidget from '../CartWidget/CartWidget'
+import './NavBar.css';
+
 const NavBar = () => {
+    const cartItemCount = 5;
         return (
             <nav>
                 <h3>HELADERIA S&H SWEET AND HEALTHY</h3>
-                <div>
+                <div className="navbar">
                     <button>Crema y Chocolate</button>
                     <button>Fresas y Crema</button>
                     <button>Sueños de Avellana</button>
+                    <CartWidget id='logo' />
+                    <span className="cart-icon">
+                        <span className="cart-count">{cartItemCount}</span>
+                        </span>
                 </div>
+                
             </nav>
         )
 }  
